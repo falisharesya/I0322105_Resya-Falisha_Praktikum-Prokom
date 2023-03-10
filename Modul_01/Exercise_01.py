21f6914cb53cd60c94a160d703f05f27036ada53
falisharesya <falisharesya@gmail.com>
2023-03-10 10:26:33 +0700

    Exercise_01

---
+++ Modul_01/Exercise_01.py
@@ -0,0 +1,15 @@
+#input
+nama = "Resya Falisha"
+nim = "I0322105"
+alamat = "Pidana Raya, Kota Tangerang, Banten"
+visualisasi = "dikenal dengan sebutan Kota Benteng"
+harapan = "kaya raya tujuh turunan"
+
+dict = {"a" : nama , "b" : nim , "c" : alamat , "d" : visualisasi , "e" : harapan}
+
+#hasil
+print("Nama saya adalah", dict["a"])
+print("NIM saya adalah ", dict["b"])
+print("Saya tinggal di", dict["c"])
+print("Dikenal dengan sebutan Kota Benteng", dict["d"])
+print("Harapan saya sebagau lulusan Teknik INdustri adalah", dict["e"])
